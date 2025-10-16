775d42be9c92c2f52997e798326e91e62cb1d33f
dbrown0738 <dbrown0738@gmail.com>
2025-10-16 11:08:31 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: Highlight active nav link
+const currentPage = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
